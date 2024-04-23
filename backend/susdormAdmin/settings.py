e5422768d9395ca427b3d6b2faf87d8fb117b612
Xavier Yuhan Liu <xzzxy2003@foxmail.com>
2024-04-23 14:23:31 +0800

    Update settings.py and delete secret key

--- backend/susdormAdmin/settings.py
+++ backend/susdormAdmin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_o2mg8m2gb83!k(veb#m7lwb@d6fnsx39!s3lb**_vten_#zgr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
